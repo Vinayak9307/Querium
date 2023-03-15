b6fb4e0a2b46db27df2fd02f9845b642e0c8aa29
Vinayak9307 <kushwahavinayak286@gmail.com>
2023-03-15 20:34:20 +0530

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -108,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.email_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
